2bf8a21d62282b353efc3f5dbe30dea8df2e025a
Zeno Scheltens <zeno.scheltens@gmail.com>
2018-10-29 14:35:34 +0100

    Rewrite succesfull

--- WeatherStationGUI.py
+++ WeatherStationGUI.py
@@ -1,74 +1,113 @@
-"""modules that are needed for tkinter"""
 from tkinter import *
 from tkinter import ttk
 from weatherstation import *
 
 
 class WeatherStationGUI(ttk.Frame):
-    """"This contains the code to draw the GUI. Its sorted in the following way:
-
-    - Style's
-        For now for debugging
-    - Frames
-        A left and a right one
-    - Left frame widget
-    - Left frame layout
-    - Right frame widget
-    - Right frame layout
-    - Notebook in Right frame
-        For every page in the notebook:
-        - Widget creation
-        - Page layout
-    """
-
-    def __init__(self, master):
+    def __init__(self, master, data):
         super().__init__(master)
-        data = WeatherStation()
-
-        """for debugging. Adding backgrounds to different objects to recognize the space they take"""
-        self.s = ttk.Style()
-        self.s.configure('red.TFrame', background='red')
-        self.s.configure('yellow.TFrame', background='yellow')
+        self.data = data
 
         # creating 2 frames
         self.master = master
-        self.left_frame = ttk.Frame(master, padding='3 3 12 12')    # left, top, right, bottom
-        self.right_frame = ttk.Frame(master, padding='3 3 12 12')    # left, top, right, bottom
+        self.left_frame = WeatherStationLeft(self.data)
+        # self.right_frame = WeatherStationRight()
 
-        self.left_frame.grid(column=1, row=1)
-        self.right_frame.grid(column=2, row=1, sticky=(N, W))
+        self.left_frame.grid(column=1, row=1, sticky=E)
+        # self.right_frame.grid(column=2, row=1, sticky=(N, W))
 
-        # widgets for the left frame
+
+class WeatherStationLeft(ttk.Frame):
+    def __init__(self, data):
+        super().__init__()
+        self.data = data
+
+        # StringVars to update labels
         self.location_selection = StringVar()
+
+        self.max_temp_location = StringVar()
+        self.min_temp_location = StringVar()
+        self.sunpower_location = StringVar()
+        self.max_wind_location = StringVar()
+        self.min_wind_location = StringVar()
+
+        self.max_temp = StringVar()
+        self.min_temp = StringVar()
+        self.sunpower = StringVar()
+        self.max_wind = StringVar()
+        self.min_wind = StringVar()
+
+        # Set a value to the StringVars
         self.location_selection.set('Locatie')
         self.location_selection.trace('w', self.change_dropdown)
-        self.extreme_weather = data.MaximumValues()
-        # self.max_temp_str = ('\nHoogste temperatuur:\n' + self.extreme_weather.max_temperature[0] + ' ' +
-        #                 str(self.extreme_weather.max_temperature[1]) + ' °C')
-        # self.min_temp_str = ('Laagste temperatuur:\n' + self.extreme_weather.min_temperature[0] + ' ' +
-        #                 str(self.extreme_weather.min_temperature[1]) + ' °C')
-        # self.sunpower_str = ('Meest Zonnige locatie:\n' + self.extreme_weather.max_sun[0] + ' ' +
-        #                 str(self.extreme_weather.max_sun[1]) + ' W/m2')
-        # self.max_wind_str = ('Het waait het hardst in:\n' + self.extreme_weather.max_windy[0] + ' ' +
-        #                 str(self.extreme_weather.max_windy[1]) + ' Bft')
-        # self.min_wind_str = ('Het waait het minst in:\n' + self.extreme_weather.min_windy[0] + ' ' +
-        #                 str(self.extreme_weather.min_windy[1]) + ' Bft')
 
-        # layout for the left frame
-        self.popup_menu = OptionMenu(self.left_frame, self.location_selection, *meetstation_locations)
-        self.popup_menu.grid(row=1, column=1, sticky=(W, E))
-        # self.max_temp_label = ttk.Label(self.left_frame, text=self.max_temp_str)
-        # self.max_temp_label.grid(column=1, row=3, sticky='W')
-        # self.min_temp_label = ttk.Label(self.left_frame, text=self.min_temp_str)
-        # self.min_temp_label.grid(column=1, row=4, sticky='W')
-        # self.sunpower_label = ttk.Label(self.left_frame, text=self.sunpower_str)
-        # self.sunpower_label.grid(column=1, row=5, sticky='W')
-        # self.max_wind_label = ttk.Label(self.left_frame, text=self.max_wind_str)
-        # self.max_wind_label.grid(column=1, row=6, sticky='W')
-        # self.min_wind = ttk.Label(self.left_frame, text=self.min_wind_str)
-        # self.min_wind.grid(column=1, row=7, sticky='W')
+        self.max_temp_location.set(self.data.max_temperature[0])
+        self.min_temp_location.set(self.data.min_temperature[0])
+        self.sunpower_location.set(self.data.max_sun[0])
+        self.max_wind_location.set(self.data.max_wind[0])
+        self.min_wind_location.set(self.data.min_wind[0])
 
-        self.display_min_max()
+        self.max_temp.set(str(self.data.max_temperature[1]) + '°C')
+        self.min_temp.set(str(self.data.min_temperature[1]) + '°C')
+        self.sunpower.set(str(self.data.max_sun[1]) + 'W/m²')
+        self.max_wind.set(str(self.data.max_wind[1]) + 'Bft')
+        self.min_wind.set(str(self.data.min_wind[1]) + 'Bft')
+
+        # Creating widgets
+        self.popup_menu = OptionMenu(self, self.location_selection, *self.data.meetstation_locations)
+
+        self.max_temp_text = ttk.Label(self, text='Highest temperature', padding='2 2 2 0')
+        self.max_temp_text_w = ttk.Label(self, textvariable=self.max_temp_location)
+        self.max_temp_text_o = ttk.Label(self, textvariable=self.max_temp)
+
+        self.min_temp_text = ttk.Label(self, text='Lowest temperature', padding='2 2 2 0')
+        self.min_temp_text_w = ttk.Label(self, textvariable=self.min_temp_location)
+        self.min_temp_text_o = ttk.Label(self, textvariable=self.min_temp)
+
+        self.sunpower_text = ttk.Label(self, text='Most sunshine', padding='2 2 2 0')
+        self.sunpower_text_w = ttk.Label(self, textvariable=self.sunpower_location)
+        self.sunpower_text_o = ttk.Label(self, textvariable=self.sunpower)
+
+        self.max_wind_text = ttk.Label(self, text='Most windy', padding='2 2 2 0')
+        self.max_wind_text_w = ttk.Label(self, textvariable=self.max_wind_location)
+        self.max_wind_text_o = ttk.Label(self, textvariable=self.max_wind)
+
+        self.min_wind_text = ttk.Label(self, text='Least windy', padding='2 2 2 0')
+        self.min_wind_text_w = ttk.Label(self, textvariable=self.min_wind_location)
+        self.min_wind_text_o = ttk.Label(self, textvariable=self.min_wind)
+
+        # Placing the widgets in the layout
+        self.popup_menu.grid(column=0, row=1, columnspan=2, sticky=(W, E))
+
+        self.max_temp_text.grid(column=0, row=3, columnspan=2, sticky='S')
+        self.max_temp_text_w.grid(column=0, row=4, sticky='W')
+        self.max_temp_text_o.grid(column=1, row=4, sticky='E')
+
+        self.min_temp_text.grid(column=0, row=5, columnspan=2, sticky='S')
+        self.min_temp_text_w.grid(column=0, row=6, sticky='W')
+        self.min_temp_text_o.grid(column=1, row=6, sticky='E')
+
+        self.sunpower_text.grid(column=0, row=7, columnspan=2, sticky='S')
+        self.sunpower_text_w.grid(column=0, row=8, sticky='W')
+        self.sunpower_text_o.grid(column=1, row=8, sticky='E')
+
+        self.max_wind_text.grid(column=0, row=9, columnspan=2, sticky='S')
+        self.max_wind_text_w.grid(column=0, row=10, sticky='W')
+        self.max_wind_text_o.grid(column=1, row=10, sticky='E')
+
+        self.min_wind_text.grid(column=0, row=11, columnspan=2, sticky='S')
+        self.min_wind_text_w.grid(column=0, row=12, sticky='W')
+        self.min_wind_text_o.grid(column=1, row=12, sticky='E')
+
+    def change_dropdown(self, *args):
+        self.selected_location = self.location_selection.get()
+        # update open tab
+        print(self.selected_location)
+
+
+class WeatherStationRight(ttk.Frame):
+    def __init__(self):
+        super().__init__()
 
         # widgets for the right frame
         self.weather_pages = ttk.Notebook(self.right_frame)
@@ -91,48 +130,12 @@ class WeatherStationGUI(ttk.Frame):
         self.weather_pages.add(self.tab5, text='tab three', compound='top')
         self.weather_pages.add(self.tab6, text='tab three', compound='top')
 
-    def display_min_max(self):
-        self.extreme_weather = MaximumValues()
-        self.max_temp_str = ('\nHoogste temperatuur:\n' + self.extreme_weather.max_temperature[0] + ' ' +
-                        str(self.extreme_weather.max_temperature[1]) + ' °C')
-        self.min_temp_str = ('Laagste temperatuur:\n' + self.extreme_weather.min_temperature[0] + ' ' +
-                        str(self.extreme_weather.min_temperature[1]) + ' °C')
-        self.sunpower_str = ('Meest zonnige locatie:\n' + self.extreme_weather.max_sun[0] + ' ' +
-                        str(self.extreme_weather.max_sun[1]) + ' W/m2')
-        self.max_wind_str = ('Het waait het hardst in:\n' + self.extreme_weather.max_windy[0] + ' ' +
-                        str(self.extreme_weather.max_windy[1]) + ' Bft')
-        self.min_wind_str = ('Het waait het minst in:\n' + self.extreme_weather.min_windy[0] + ' ' +
-                        str(self.extreme_weather.min_windy[1]) + ' Bft')
-
-        self.max_temp_label = ttk.Label(self.left_frame, text=self.max_temp_str)
-        self.max_temp_label.grid(column=1, row=3, sticky='W')
-        self.min_temp_label = ttk.Label(self.left_frame, text=self.min_temp_str)
-        self.min_temp_label.grid(column=1, row=4, sticky='W')
-        self.sunpower_label = ttk.Label(self.left_frame, text=self.sunpower_str)
-        self.sunpower_label.grid(column=1, row=5, sticky='W')
-        self.max_wind_label = ttk.Label(self.left_frame, text=self.max_wind_str)
-        self.max_wind_label.grid(column=1, row=6, sticky='W')
-        self.min_wind = ttk.Label(self.left_frame, text=self.min_wind_str)
-        self.min_wind.grid(column=1, row=7, sticky='W')
-        print('done update')
-        self.after(10000, self.display_min_max)
-
-    def change_dropdown(self, *args):
-        self.selected_location = self.location_selection.get()
-        # update open tab
-        print(self.selected_location)
-
-
-class LeftFrame(Tk):
-    def __init__(self):
-
-
-
 
 def main():
     root = Tk()
-    root.title('Weatherstations Netherlands')
-    app = WeatherStationGUI(root)
+    root.title('Weatherstation Netherlands')
+    data = WeatherStation()
+    app = WeatherStationGUI(root, data)
     app.mainloop()
 
 
